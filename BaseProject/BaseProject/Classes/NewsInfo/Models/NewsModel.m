727031b84fe32d35d0b805a0bc8182f492393d9f
Gmc0601 <cc@MingchenGengdeMacBook-Pro.local>
2017-06-27 18:32:39 +0800

    AddAll

---
+++ BaseProject/BaseProject/Classes/NewsInfo/Models/NewsModel.m
@@ -0,0 +1,13 @@
+//
+//  NewsModel.m
+//  BaseProject
+//
+//  Created by cc on 2017/6/27.
+//  Copyright © 2017年 cc. All rights reserved.
+//
+
+#import "NewsModel.h"
+
+@implementation NewsModel
+
+@end
