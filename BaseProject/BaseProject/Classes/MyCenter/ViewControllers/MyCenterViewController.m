c9414bf9e6c53cb43a9f8fb2f26b6a37c3ada871
smilelifu <401191608@qq.com>
2017-06-27 16:22:26 +0800

    上传个人中心

--- BaseProject/BaseProject/Classes/MyCenter/ViewControllers/MyCenterViewController.m
+++ BaseProject/BaseProject/Classes/MyCenter/ViewControllers/MyCenterViewController.m
@@ -8,7 +8,7 @@
 
 #import "MyCenterViewController.h"
 
-@interface MyCenterViewController ()
+@interface MyCenterViewController ()<UITableViewDelegate,UITableViewDataSource>
 
 @end
 
@@ -27,12 +27,64 @@
  */
 -(UITableView *)setIMagetable{
     if (!_setIMagetable) {
-        
-        
-        
+        self.setIMagetable=[[UITableView alloc]initWithFrame:CGRectMake(0,0,kScreenW,kScreenH) style:UITableViewStyleGrouped];
+        self.setIMagetable.showsVerticalScrollIndicator=NO;
+        self.setIMagetable.separatorInset=UIEdgeInsetsMake(0,0,0,0);
+        self.setIMagetable.delegate=self;
+        self.setIMagetable.dataSource=self;
+        [self.view addSubview:self.setIMagetable];
     }
     return _setIMagetable;
 }
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 50;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    if (section==0) {
+        return 100;
+    }else{
+        return 0.01;
+    }
+}
+
+- (nullable UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    UIView *setView=[[UIView alloc]init];
+    setView.backgroundColor=UIColorFromHex(0xf6f6f6);
+    return setView;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (section==0) {
+        return 4;
+    }else if (section==1){
+        return 5;
+    }
+    return YES;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *identifier = @"UITableViewCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell==nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        cell.backgroundColor=[UIColor whiteColor];
+        
+    }
+    return cell;
+}
+
+
+
+
 
 
 @end
