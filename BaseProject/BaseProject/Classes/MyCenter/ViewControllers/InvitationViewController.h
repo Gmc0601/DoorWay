a10d6c6a7c74799563ac632899708494532b04a1
smilelifu <401191608@qq.com>
2017-06-28 14:17:15 +0800

    上传个人中心

---
+++ BaseProject/BaseProject/Classes/MyCenter/ViewControllers/InvitationViewController.h
@@ -0,0 +1,13 @@
+//
+//  InvitationViewController.h
+//  BaseProject
+//
+//  Created by li on 2017/6/28.
+//  Copyright © 2017年 cc. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface InvitationViewController : BaseViewController
+
+@end
---
+++ BaseProject/BaseProject/Classes/MyCenter/ViewControllers/InvitationViewController.m
@@ -0,0 +1,29 @@
+//
+//  InvitationViewController.m
+//  BaseProject
+//
+//  Created by li on 2017/6/28.
+//  Copyright © 2017年 cc. All rights reserved.
+//
+
+#import "InvitationViewController.h"
+
+@interface InvitationViewController ()
+
+@end
+
+@implementation InvitationViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = UIColorFromHex(0xf6f6f6);
+    
+    [self setCustomerTitle: @"我的邀请码"];
+
+    
+}
+
+
+@end
